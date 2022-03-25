b49a69ade9d61bfd8fb44844ab2dfee92dfd2a01
sande <sande@DESKTOP-7K9D7AE>
2022-03-25 10:05:12 -0700

    Added Welcome Class

---
+++ src/com/datastructures/sandy/Welcome.java
@@ -0,0 +1,12 @@
+package com.datastructures.sandy;
+
+public class Welcome {
+public static void main(String[] args) {
+	System.out.println("Welcome to DataStructures Prepartion");
+	
+	System.out.println("Goal is to understand and implement different datastructures and complete the tasks specified in the Document");
+	
+	System.out.println("Lets get started!");
+	
+}
+}
